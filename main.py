2e06e568bdb7030452fe8fbad593b168c4fced6f
Mahal <mahlamjspace@gmail.com>
2026-07-06 18:54:24 +0330

    Add menu selection

--- main.py
+++ main.py
@@ -8,3 +8,20 @@ print("3. Mole to Gram Converter")
 print("4. Percent Composition")
 print("5. Exit")
 
+choice = input("Choose an option (1-5): ")
+print("You selected:", choice)
+
+
+
+if choice == "1":
+    print("Molar Mass Calculator")
+elif choice == "2":
+    print("Gram to Mole Converter")
+elif choice == "3":
+    print("Mole to Gram Converter")
+elif choice == "4":
+    print("Percent Composition")
+elif choice == "5":
+    print("Goodbye!")
+else:
+    print("Invalid option")
