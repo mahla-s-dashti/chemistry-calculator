9095db77bf9e0d80bc80739691951a844f2991b9
Mahal <mahlamjspace@gmail.com>
2026-07-06 18:50:31 +0330

    Add menu selection

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-print("===================================")
-print(" Welcome to Chemistry Calculator ")
-print("===================================")
-print("Developed by Mahla Sadat Dashti")
+print("="*40)
+print("Chemistry Calculator")
+print("="*40)
+
+print("1. Calculate Molar Mass")
+print("2. Gram to Mole Converter")
+print("3. Mole to Gram Converter")
+print("4. Percent Composition")
+print("5. Exit")
+
