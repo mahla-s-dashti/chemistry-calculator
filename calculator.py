5e0a9534bf805357550c0059c17877d8daad814c
Mahal <mahlamjspace@gmail.com>
2026-08-23 19:45:28 +0330

    Implement molar mass calculator

--- calculator.py
+++ calculator.py
@@ -1,2 +1,17 @@
+from elements import ATOMIC_MASSES
+
 def calculate_molar_mass(parsed_formula):
-    pass
+    """
+    Calculate the molar mass of a chemical formula.
+    Parameters:
+        parsed_formula (dict): A dictionary which contains element symbols and their counts.
+    Returns:
+        float: The molar mass in g/mol.
+    """
+
+    total_mass = 0.0
+
+    for element , count in parsed_formula.items():
+        total_mass += ATOMIC_MASSES[element] * count
+
+    return total_mass
